c61d103dfd3ae63409df53a7581c847e5300f346
Jimit Shah <jshah11@icloud.com>
2018-05-05 18:24:50 -0700

    Organize project using MVC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -14,7 +14,8 @@ class TodoListViewController: UITableViewController {
   
   // MARK: Properties
   
-  var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+  var itemArray = [Item]()
+  
   let defaults = UserDefaults.standard
   
   // MARK: Actions
@@ -44,7 +45,19 @@ class TodoListViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+    let newItem = Item()
+    newItem.title = "Find Mike"
+    itemArray.append(newItem)
+    
+    let newItem2 = Item()
+    newItem2.title = "Buy Eggos"
+    itemArray.append(newItem2)
+    
+    let newItem3 = Item()
+    newItem3.title = "Destroy Demogorgon"
+    itemArray.append(newItem3)
+    
+    if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
       itemArray = items
     }
   }
@@ -55,7 +68,9 @@ class TodoListViewController: UITableViewController {
     if string.isEmpty {
       return
     }
-    itemArray.append(string.capitalized)
+    let newItem = Item()
+    newItem.title = string.capitalized
+    itemArray.append(newItem)
     defaults.set(itemArray, forKey: "TodoListArray")
     tableView.reloadData()
   }
@@ -70,8 +85,13 @@ extension TodoListViewController {
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let item = itemArray[indexPath.row]
+    
     let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-    cell.textLabel?.text = itemArray[indexPath.row]
+    
+    cell.textLabel?.text = item.title
+    cell.accessoryType = item.done ? .checkmark : .none
     return cell
   }
 }
@@ -82,9 +102,10 @@ extension TodoListViewController {
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
-    if let cell = tableView.cellForRow(at: indexPath) {
-      cell.accessoryType = (cell.accessoryType == .none) ? .checkmark : .none
-    }
+    itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+    
+    tableView.reloadData()
+    
     tableView.deselectRow(at: indexPath, animated: true)
   }
   
